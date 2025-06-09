771a61ee698fe00f13368565f0d21f80af53436f
yashasreemuppalla <yashasreemuppalla@gmail.com>
2025-06-09 16:16:56 +0530

    modified:   src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import Header from "./header";
+import Header from "./Header";
 import Footer from "./Footer";
 import Home from "./Home";
 function App() {
