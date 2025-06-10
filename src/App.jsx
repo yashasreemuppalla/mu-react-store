1aa46ecc6cc7ab09de328d9ae25d616049d46bb8
yashasreemuppalla <yashasreemuppalla@gmail.com>
2025-06-10 10:39:06 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,23 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 import Header from "./Header";
 import Footer from "./Footer";
+import Login from "./Login";
+import Register from "./Register";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 function App() {
-  console.log("App component rendered");
   return (
     <div>
-      <Header />
-      <Home />
-      <Footer />
+      <BrowserRouter>
+        <Header name="mu-react-store" />
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,15 +1,21 @@
+
 import React from "react";
 import "./Header.css";
-export default function Header() {
+import { Link } from "react-router-dom";
+export default function Header({ name }) {
   return (
-    <div className="App-Header-Row">
+    <div className="App-Header-Row" style={{ backgroundColor: "pink" }}>
       <div>
-        <h1>Mu-React-Store</h1>
+        <h1>{name}</h1>
       </div>
       <div>
         <ul>
-          <li>Home</li>
-          <li>Login</li>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
         </ul>
       </div>
     </div>
